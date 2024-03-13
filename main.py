9bfa24e952e7dae9200415e3ed2e4b830d3259ea
Sami Mhidia <sami.mhidia@jarvisator.com>
2024-03-13 06:44:42 +0100

    updated before doc

--- main.py
+++ main.py
@@ -5,23 +5,12 @@ from authentication.api_key import get_basic_api_key
 from database import save_item, get_db, get_item_by_id, list_items, update_item_by_id, delete_item_by_id
 from models import Item
 
-sentry_sdk.init(
-    dsn="https://e706f3f7e7f87c42358f0fe9c64e3414@o4504293372985344.ingest.sentry.io/4506807097491456",
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    traces_sample_rate=1.0,
-    # Set profiles_sample_rate to 1.0 to profile 100%
-    # of sampled transactions.
-    # We recommend adjusting this value in production.
-    profiles_sample_rate=1.0,
-)
-
-
 app = FastAPI(
     title="Item Management API for GPTs",
     description="This is a simple API to manage items. It is part of a tutorial for custom actions in GPTs.",
     version="1.0.0",
 )
+
 
 @app.get("/")
 async def read_root():
